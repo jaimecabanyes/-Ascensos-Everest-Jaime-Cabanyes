2821ae2fa16d98bce483b0d289e4455859314726
jaimecabanyes <jaimecabanyes@gmail.com>
2021-08-31 01:33:02 +0200

    cambios

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import collections
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import src.funciones_limpieza as fn
 
 ascensos_everest = pd.read_csv("Mt_Everest_Ascent_Data.csv")
+
+final = fn.cleaning_dataframe(ascensos_everest)
+print(final)
